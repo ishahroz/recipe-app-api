890c98fc4ff51b583c624584c0fc34d4b5d3f948
Shahroz Ahmad <97090106+ishahroz@users.noreply.github.com>
2023-04-19 20:56:53 +0500

    chore: [FINAL SETTINGS WORKING] updated docker-compose to incorporate DB command and also updated Github workflows to incorporate DB command (#5)

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-dlg(gu)f#qug6+@p+w00to#&!6v1n@muaeod2b**gcmfj)0%0g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['0.0.0.0']
 
 # Application definition
 
